fa0e66f2e3f7fc46dfda82a830e04f7694651c09
David Konsumer <konsumer@jetboystudio.com>
2023-07-14 13:27:20 -0700

    use cache better, findByUrl

--- src/index.js
+++ src/index.js
@@ -11,43 +11,44 @@ export default class Content {
   constructor (collection, collectionName, basePath = '.') {
     this.collection = collection
     this.basePath = basePath
-
     this.collection.name = collectionName
-
     for (const f of Object.keys(this.collection.fields)) {
       this.collection.fields[f].name = f
     }
   }
 
   // get all filenames that matches files pattern in def
-  async list (grabItems = false) {
-    const list = (await glob(this.basePath + this.collection.files)).map(f => '/' + f)
-    if (grabItems) {
-      return Promise.all(list.map(f => this.get(f)))
-    } else {
-      return list
+  async list () {
+    if (cache[this.collection.name]) {
+      return cache[this.collection.name]
     }
+    const list = (await glob(this.basePath + this.collection.files)).map(f => '/' + f)
+    cache[this.collection.name] = await Promise.all(list.map(f => {
+      const { data, content, excerpt } = matter(readFileSync(this.basePath + filename, 'utf8'), { excerpt: true })
+      data.url = tt(this.collection.url, { ...data, filename, content })
+      data.excerpt = excerpt
+      data.filename = filename
+      data.children = content
+
+      // post-process data
+      for (const f of Object.keys(data)) {
+        const field = this.collection.fields[f]
+        if (field && loadProcessors[field.type]) {
+          data[f] = loadProcessors[field.type](data[f])
+        }
+      }
+    }))
+    return cache[this.collection.name]
   }
 
   // get a single article, keyed by filename
   async get (filename) {
-    if (cache[filename]) {
-      return cache[filename]
-    }
-    const { data, content, excerpt } = matter(readFileSync(this.basePath + filename, 'utf8'), { excerpt: true })
-    data.url = tt(this.collection.url, { ...data, filename, content })
-    data.excerpt = excerpt
-    data.filename = filename
-    cache[filename] = { ...data, children: content }
+    const list = await this.list()
+    return list.find(a => a.filename === filename)
+  }
 
-    // post-process data
-    for (const f of Object.keys(cache[filename])) {
-      const field = this.collection.fields[f]
-      if (field && loadProcessors[field.type]) {
-        cache[filename][f] = loadProcessors[field.type](cache[filename][f])
-      }
-    }
-
-    return cache[filename]
+  async getByUrl (url) {
+    const list = await this.list()
+    return list.find(a => a.url === url)
   }
 }
